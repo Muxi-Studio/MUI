b1c2af27de9cdb86f6a0d7d04be2906f5eaf62bc
zxc0328 <120797887@qq.com>
2017-02-28 22:26:29 +0800

    Start code for toast

---
+++ src/components/toast/index.js
@@ -0,0 +1,9 @@
+import Toast from "./toast.vue"
+
+// shuold expose API here
+// this.toast.info(content, duration, onClose)
+// this.toast.warn(content, duration, onClose)
+// this.toast.success(content, duration, onClose)
+// this.toast.error(content, duration, onClose)
+
+export default Toast
---
+++ src/components/toast/toast.vue
@@ -0,0 +1,31 @@
+<template>
+    <div :class="m-toast">
+        {{ content }}
+    </div>
+</template>
+<script>
+export default {
+    name: "m-toast",
+    props: {
+        type: {
+            type: String,
+            default: "primary",
+        },
+        content: {
+            type: String,
+            default: "",
+        },
+        onClose: {
+            type: Function,
+            default: () => {},
+        },
+    },
+}
+</script>
+<style lang="scss">
+@import '../../../scss/common/color.scss';
+@import '../../../scss/common/font.scss';
+.m-toast {
+    
+}
+</style>
